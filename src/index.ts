9bc9324e41d52906032b10ccb52b61f1c37fb20a
Tanmaykk <tanmaykachroo@gmail.com>
2022-11-08 02:50:44 +0530

    Path updated

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { email, highlightsParser, randomHighlightsSlice } from "./utils";
 import { config } from "dotenv";
-
+import path from 'path'
 config();
 
 const run = async () => {
-  const allHighlights = await highlightsParser("./src/test/My Clippings.txt");
+  const clippingsPath  = path.join("src", "test","My Clippings.txt");
+  const allHighlights = await highlightsParser(clippingsPath);
   const randomSlice = randomHighlightsSlice(allHighlights, 2);
 
   await email(randomSlice);
