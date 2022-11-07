7beebacf608bfbb34e69c981508b1c980218cd41
Tanmaykk <tanmaykachroo@gmail.com>
2022-11-07 18:25:33 +0530

    Converts to array of objects of highlights

--- src/utils.ts
+++ src/utils.ts
@@ -3,5 +3,21 @@ import {readFile} from 'fs/promises'
 
 export const highlightsParser = async (path:PathLike )=>{
     const content = await readFile(path,'utf-8');
-    console.log({content});
+    const arr = content.split("==========");
+    
+    const newarr = arr.map(ele=>{
+        const splitAndRemoveEmptyLines =  ele.split('\r\n').filter(f=>f.length>0);
+        if (splitAndRemoveEmptyLines.length !=3){
+            return null;
+        }
+        const highlightObj = {
+            bookTitle:splitAndRemoveEmptyLines[0],
+            content:splitAndRemoveEmptyLines[2],
+            details:splitAndRemoveEmptyLines[1]?.slice(2)
+        }
+
+        return highlightObj;
+    }).filter(Boolean)
+    
+    console.log({newarr});
 }
